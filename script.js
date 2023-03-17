b7e238b2f837a26c02774e51946ed052bf498be5
NirajGadara <120002683+NirajGadara@users.noreply.github.com>
2023-03-17 11:44:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -81,123 +81,13 @@ submit.addEventListener('click', () => {
     const checkAns = getcheckAns();
     console.log(checkAns);
 
-    if(checkAns === quizDBDB[questionNum].ans){
+    if(checkAns === quizDB[questionNum].ans){
         score++;
     };
 
     questionNum++;
-    if(questionNum < quizDBDB.length){
+    if(questionNum < quizDB.length){
         loadQuestion();
     }
 
 })
-
-
-// let quizDB =
-// [
-//     {
-//         question: "Which of the following can read and render HTML web pages",
-//         a: "Server",
-//         b: "head Tak",
-//         c: "web browser",
-//         d: "empty",
-//         correct: "c",
-         
-//     },
-//     {
-//         question: "Identify the range of byte data types in JavaScript.",
-//         a: "-10 to 9",
-//         b: "- 128 to 127",
-//         c: "-32768 to 32767",
-//         d: "-2147483648 to 2147483647",
-//         correct:"b",
-//     },
-//     {
-//         question: " Among the following operators identify the one which is used to allocate memory to array variables in JavaScript.",
-//         a: "new",
-//         b: "new malloc",
-//         c: "alloc",
-//         d: "malloc",
-//         correct:"a",
-//     },
-//     {
-//         question: "Simple network management protocol uses which of the following port number",
-//         a: "164",
-//         b: "163", 
-//         c: "160",
-//         d: "161",
-//         correct:"d",
-//     }
-// ];
-// let index = 0;
-// let question = document.getElementById("question");
-// let radio = document.querySelectorAll("input[type='radio']")
-// let num = document.getElementById("num");
-// let t = document.getElementById("total");
-// let tt = quizDB.length;
-// let c = 0;
-// let w = 0; 
-// function Question()
-// {
-//     let data = quizDB[index];
-//     if(tt === index)
-//     {
-//        return end();
-//     }
-//     reset();
-//     console.log("hii");
-//     num.innerText = index+1;
-//     t.innerText = tt;
-//     question.innerHTML = `${index + 1} ${data.question}`;
-//     radio[0].nextElementSibling.innerText = data.a;
-//     radio[1].nextElementSibling.innerText = data.b;
-//     radio[2].nextElementSibling.innerText = data.c;
-//     radio[3].nextElementSibling.innerText = data.d;
-// }
-// function end()
-// {
-//     let con = document.getElementsByClassName("main_section")[0];
-//     con.innerHTML = `
-//     <div class="c1">
-//         <h1>${c} Right From ${tt}</h1>
-//     </div>
-//     `;
-// }
-// function reset()
-// {
-//     radio.forEach(
-//         (input) => {
-//             input.checked = false;
-//         }
-//     )
-// }
-// function Sub()
-// {
-//     let data = quizDB[index];
-//     let a = ans();
-//     if( a === data.correct)
-//     {
-//         c++;
-//     }
-//     else
-//     {
-//         w++;
-//     }
-//     index++;
-//     Question();
-// }
-// function ans()
-// {
-//     let a;
-//     radio.forEach(
-//         (input) =>
-//         {
-//             if(input.checked)
-//             {
-//                 a = input.value;
-//             }
-//         }
-//     )
-//     return a;
-// }
-// Question(index);
